4ff2ba188d9e22e20ad123317ae3626956632ea5
Roman Bachmann <Roman.Bachmann@swisscom.com>
2020-07-03 12:49:30 +0200

    Make use of graceful-shutdown config and appropriate ApplicationEvents

--- src/main/java/com/swisscom/clouds/services/PodLifecycleProcessor.java
+++ src/main/java/com/swisscom/clouds/services/PodLifecycleProcessor.java
@@ -2,11 +2,12 @@ package com.swisscom.clouds.services;
 
 import com.swisscom.clouds.callbacks.Callback;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -26,15 +27,15 @@ public class PodLifecycleProcessor {
 
     }
 
-    @PostConstruct
-    private void postConstruct() {
+    @EventListener(ApplicationStartedEvent.class)
+    public void applicationStarted() {
         Flux.fromIterable(callbackImplementations)
                 .flatMap(Callback::onStartup)
                 .subscribe();
     }
 
-    @PreDestroy
-    private void preDestroy() {
+    @EventListener(ContextClosedEvent.class)
+    public void contextClosed() {
         Flux.fromIterable(callbackImplementations)
                 .flatMap(Callback::onShutdown)
                 .blockLast();
