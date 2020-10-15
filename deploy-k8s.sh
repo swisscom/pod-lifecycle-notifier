fa15be152b22f41dfd1ec1855005483dd1e452cc
Roman Bachmann <Roman.Bachmann@swisscom.com>
2020-10-15 13:33:31 +0200

    Ensure <none> is never picked as latest tag

--- deploy-k8s.sh
+++ deploy-k8s.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-LATEST_TAG=$(docker images robachmann/pod-lifecycle-notifier  --format "{{.Tag}}" | sort -r | head -n 1) && echo $LATEST_TAG
+LATEST_TAG=$(docker images robachmann/pod-lifecycle-notifier --format "{{.Tag}}" | grep -v "<none>" | sort -r | head -n 1) && echo $LATEST_TAG
 sed -i '' "s/pod-lifecycle-notifier:.*/pod-lifecycle-notifier:$LATEST_TAG/g" k8s/deployment.yml
 CLUSTER_NAME=$(kubectl config current-context) && echo $CLUSTER_NAME
 sed -i '' "s/clusterName:.*/clusterName: $CLUSTER_NAME/g" k8s/secret-callback.yml
