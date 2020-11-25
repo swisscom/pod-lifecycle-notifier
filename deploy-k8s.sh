df4e109452d2a71baf5fc7cb187e2da060da3728
Roman Bachmann <Roman.Bachmann@swisscom.com>
2020-11-25 10:18:51 +0100

    Rename deployment to daemonset

--- deploy-k8s.sh
+++ deploy-k8s.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 LATEST_TAG=$(docker images robachmann/pod-lifecycle-notifier --format "{{.Tag}}" | grep -v "<none>" | sort -r | head -n 1) && echo $LATEST_TAG
-sed -i '' "s/pod-lifecycle-notifier:.*/pod-lifecycle-notifier:$LATEST_TAG/g" k8s/deployment.yml
+sed -i '' "s/pod-lifecycle-notifier:.*/pod-lifecycle-notifier:$LATEST_TAG/g" k8s/daemonset.yml
 CLUSTER_NAME=$(kubectl config current-context) && echo $CLUSTER_NAME
 sed -i '' "s/clusterName:.*/clusterName: $CLUSTER_NAME/g" k8s/secret-callback.yml
 kubectl apply -f k8s/secret-callback.yml
-kubectl apply -f k8s/deployment.yml
+kubectl apply -f k8s/daemonset.yml
