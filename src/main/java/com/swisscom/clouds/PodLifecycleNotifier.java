83ac5ca4a593413a066b6626496e03a8d501112d
Roman Bachmann <Roman.Bachmann@swisscom.com>
2020-11-25 12:46:43 +0100

    Add support for Spring Native Image

--- src/main/java/com/swisscom/clouds/PodLifecycleNotifier.java
+++ src/main/java/com/swisscom/clouds/PodLifecycleNotifier.java
@@ -3,7 +3,7 @@ package com.swisscom.clouds;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(proxyBeanMethods = false)
 public class PodLifecycleNotifier {
 
     public static void main(String[] args) {
