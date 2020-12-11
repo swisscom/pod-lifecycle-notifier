50315b1b0c041ef3a50cda0b9d420610b0e48e47
Roman Bachmann <Roman.Bachmann@swisscom.com>
2020-12-11 16:13:28 +0100

    Cleanup

--- build-and-publish.sh
+++ build-and-publish.sh
@@ -2,3 +2,5 @@
 ./gradlew bootBuildImage
 LATEST_TAG=$(docker images robachmann/pod-lifecycle-notifier --format "{{.Tag}}" | grep -v "<none>" | sort -r | head -n 1) && echo $LATEST_TAG
 docker push robachmann/pod-lifecycle-notifier:$LATEST_TAG
+
+# to test locally, you can use docker run -it --rm -p 8080:8080 -e CALLBACK_MS_TEAMS_URI=https://httpbin.org/post robachmann/pod-lifecycle-notifier:$LATEST_TAG
