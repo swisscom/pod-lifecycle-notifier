11df0528f48289a7f997cc380a3d8d88ee59fc14
Roman Bachmann <Roman.Bachmann@swisscom.com>
2020-12-11 17:23:58 +0100

    Add run-locally.sh script

--- run-locally.sh
+++ run-locally.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-./gradlew bootBuildImage
 LATEST_TAG=$(docker images robachmann/pod-lifecycle-notifier --format "{{.Tag}}" | grep -v "<none>" | sort -r | head -n 1) && echo $LATEST_TAG
-docker push robachmann/pod-lifecycle-notifier:$LATEST_TAG
+docker run -it --rm -p 8080:8080 -e CALLBACK_MS_TEAMS_URI=https://httpbin.org/post robachmann/pod-lifecycle-notifier:$LATEST_TAG
