e60e37fff6931694cce4386a79018376faa706dd
Roman Bachmann <Roman.Bachmann@swisscom.com>
2020-12-11 16:11:30 +0100

    add --enable-https flag for webflux-starter, remove obsolete 'proxyBeanMethods = false' parameter

--- src/main/java/com/swisscom/clouds/PodLifecycleNotifier.java
+++ src/main/java/com/swisscom/clouds/PodLifecycleNotifier.java
@@ -3,7 +3,7 @@ package com.swisscom.clouds;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(proxyBeanMethods = false)
+@SpringBootApplication
 public class PodLifecycleNotifier {
 
     public static void main(String[] args) {
