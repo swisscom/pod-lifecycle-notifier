19f59f8c233d9546fb5bad02012ea7f19cdb50c8
Roman Bachmann <Roman.Bachmann@swisscom.com>
2021-04-05 19:39:08 +0200

    Specify namespace in deploy script

--- deploy-k8s.sh
+++ deploy-k8s.sh
@@ -2,4 +2,4 @@
 CLUSTER_NAME=$(kubectl config current-context) && echo "CLUSTER_NAME=$CLUSTER_NAME"
 sed -i '' "s/KUBERNETES_CLUSTER: .*/KUBERNETES_CLUSTER: \"$CLUSTER_NAME\"/g" k8s/notifier-config.yml
 kubectl apply -f k8s/
-kubectl rollout restart daemonset notifier-daemonset
+kubectl rollout restart daemonset notifier-daemonset -n pod-lifecycle-notifier
